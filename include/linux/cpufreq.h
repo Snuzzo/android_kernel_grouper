4fab860ca3f2e7976a17ce634ca928882eae3129
Snuzzo <jrracinfan@gmail.com>
2013-02-20 05:08:03 -0500

    Revert "Added Badass Governor"
    This reverts commit c8e98f988e5d0973b5835a3d16446b07871b99e0.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -374,9 +374,6 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_dancedance)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BADASS)
-extern struct cpufreq_governor cpufreq_gov_badass;
-#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_badass)
 #endif
 
 
